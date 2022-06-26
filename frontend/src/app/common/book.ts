d902b5b95b4c6ba7e82d6bc43614857ba3bfa3d3
V-Teles <vhbelo@hotmail.com>
2022-06-26 23:51:32 +0200

    Book class implementation

--- frontend/src/app/common/book.ts
+++ frontend/src/app/common/book.ts
@@ -1,2 +1,14 @@
 export class Book {
+
+    id!: number
+    title!: string
+    author!: string
+    publisher!: string
+    year!: number
+    language!: string
+    pages!: string
+    availability!: boolean
+    dateCreated!: Date
+    lastUpdate!: Date
+
 }
