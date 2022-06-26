586ea8c4a07a586da79c4f6acd99f8743aa80dec
V-Teles <vhbelo@hotmail.com>
2022-06-26 23:42:26 +0200

    Creation of components, services and book class

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateBookComponent } from './components/create-book/create-book.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { LibraryComponent } from './components/library/library.component';
+import { UpdateBookComponent } from './components/update-book/update-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBookComponent,
+    BookDetailsComponent,
+    LibraryComponent,
+    UpdateBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
