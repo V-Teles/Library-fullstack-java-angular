6f5a8ae3b46fbd0e4fdb52a2d5d948c7e1af4b18
V-Teles <vhbelo@hotmail.com>
2022-06-29 22:00:25 +0200

    Routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { CreateBookComponent } from './components/create-book/create-book.component';
+import { LibraryComponent } from './components/library/library.component';
+import { UpdateBookComponent } from './components/update-book/update-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'library', component: LibraryComponent},
+  {path: 'create-book', component: CreateBookComponent},
+  {path: '', redirectTo: 'library', pathMatch: 'full'},
+  {path: 'update-book/:id', component: UpdateBookComponent},
+  {path: 'book-details/:id', component: BookDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
