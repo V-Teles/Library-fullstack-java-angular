276ba63af61d08eefa743d97591a0105dda040c9
V-Teles <vhbelo@hotmail.com>
2022-07-07 19:17:02 +0200

    Search feature implemented

--- backend/src/main/java/com/victor/backend/repository/BookRepository.java
+++ backend/src/main/java/com/victor/backend/repository/BookRepository.java
@@ -1,10 +1,17 @@
 package com.victor.backend.repository;
 
 import com.victor.backend.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "bookSearch", path = "book") //Json entry's name & path
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
